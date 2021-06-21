a89882088cb9ce76fe198355b5fa9d33bd79b6f0
herongyikpsh <86100970+herongyikpsh@users.noreply.github.com>
2021-06-22 00:25:25 +0600

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -523,9 +523,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirror Bot Search',
-                                 author_name='Slam Mirror Bot',
-                                 author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                 title = 'Onioneers Cloud Search',
+                                 author_name='Ridoxin',
+                                 author_url='https://t.me/ridoxin',
                                  html_content=content)
         return
 
